c0ecfa438f9784c2732b8918c5e10b6b41b21a94
Reahu Seak <reahu.freedom@gmail.com>
2023-07-12 08:04:13 +0700

    update booking history

--- app/Http/Controllers/Endpoint/EpBookingController.php
+++ app/Http/Controllers/Endpoint/EpBookingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Endpoint;
 use App\Http\Controllers\Controller;
 use App\Models\Models\Booking;
 use App\Models\Models\Bus;
+use App\Models\Models\BusTicket;
 use App\Models\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -50,7 +51,7 @@ class EpBookingController extends Controller
         $user = User::query()->findOrFail($booking['user_id'])->toArray();
         $bus = Bus::query()->findOrFail($booking['bus_id']);
         if($bus) {
-            $buses = (new EpBusController())->busFormat($bus);
+            $buses = $this->busFormat($bus);
         } else {
             $buses = [];
         }
@@ -111,5 +112,29 @@ class EpBookingController extends Controller
 
         $booking = Booking::query()->where('user_id', Auth::id())->where('bus_id', $data['bus_id'])->get();
         return $this->success($booking, 'Booking successfully');
+    }
+
+    public function busFormat(Bus $bus): array
+    {
+        $user = User::query()->findOrFail($bus['user_id']);
+        return [
+            'id' => $bus['id'],
+            'user_id' => [$user],
+            'plate_number' => $bus['plate_number'],
+            'bus_type' => $bus['bus_type'],
+            'capacity' => $bus['capacity'],
+            'book_seat' => $bus['book_seat'],
+            'bus_ticket' => $this->getBusTicket($bus['id'], $bus['created_at']),
+            'status' => $bus['status'],
+            'created_at' => $bus['created_at'],
+            'updated_at' => $bus['updated_at']
+        ];
+    }
+
+    public function getBusTicket($busID , $createdAt): \Illuminate\Database\Eloquent\Collection|array
+    {
+        $nextDay = $createdAt->addDay(1)->setTime(0,0,0,0);
+        $thisDay = $createdAt->setTime(0,0,0,0);
+        return BusTicket::query()->where('bus_id', $busID)->where('created_at', '<', $thisDay)->where('created_at', '<', $nextDay)->get()->toArray();
     }
 }
